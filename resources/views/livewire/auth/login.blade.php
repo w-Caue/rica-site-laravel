d1e500e6663c8b3688b92cd2457c36eb826c8045
Samuel <85837877+samuelbezerra23@users.noreply.github.com>
2025-03-12 16:52:47 -0300

    ajuste

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -6,20 +6,22 @@
             <!-- Cnpj -->
             <div>
                 <x-form.label value="Cnpj" />
-                <x-form.input wire:model="cnpj" placeholder="00.000.000/0000-00"
+                <x-form.input placeholder="00.000.000/0000-00"
                     x-mask:dynamic="
-                $input.startsWith('18') ? '99.999.999/9999-99' : '99.999.999/9999-99'" required/>
+                $input.startsWith('18') ? '99.999.999/9999-99' : '99.999.999/9999-99'" />
             </div>
 
             <!-- Password -->
             <div class="relative">
                 <x-form.label value="Senha" />
-                <x-form.input wire:model="senha" type="password" placeholder="********" required/>
+                <x-form.input type="password" placeholder="********" />
             </div>
 
             <div class="flex justify-center">
+                {{-- <x-form.button" label="Entrar"/> --}}
+
                 <button
-                    class="text-sm uppercase flex items-center font-semibold justify-center gap-1 text-gray-600 bg-blue-200 rounded-md p-2 transition-all hover:scale-95 focus:outline-blue-600">
+                    class="flex items-center font-semibold justify-center gap-1 text-blue-400 bg-blue-200 rounded-xl p-2 transition-all hover:scale-95 focus:outline-blue-600">
                     Entrar
                 </button>
             </div>
@@ -28,7 +30,7 @@
         <div class="space-x-1 text-center text-sm text-zinc-600 dark:text-zinc-400">
             Não é cliente Rica?
             {{-- <flux:link href="{{ route('register') }}" wire:navigate>Inscreva-se</flux:link> --}}
-            <a href="" wire:navigate class="hover:text-gray-500 hover:underline">Inscreva-se</a>
+            <a href="{{ route('register') }}" wire:navigate class="hover:text-gray-500 hover:underline">Inscreva-se</a>
         </div>
     </div>
 </div>
